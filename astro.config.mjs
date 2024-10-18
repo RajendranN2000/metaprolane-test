fc0e35a9ea96bb1dba4df1e5d00e4abae70d2649
rajendran <rajendran@shamlatech.com>
2024-10-18 17:29:12 +0530

    added robot-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import dotenv from 'dotenv';
-
+import robotsTxt from 'astro-robots-txt';
 dotenv.config();
 // https://astro.build/config
 export default defineConfig({
     site:"https://metaprolane-development.vercel.app/",
-    integrations: [react(),sitemap()],
+    integrations: [react(),sitemap(),robotsTxt()],
 });
