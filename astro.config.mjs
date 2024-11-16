1cef73df9de13375e03c49ae885cc81ad3534a0d
Developer <info@shamlatech.com>
2024-11-16 16:42:25 +0530

    Compress the Astro elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import dotenv from 'dotenv';
 import robotsTxt from 'astro-robots-txt';
+import compress from 'astro-compress';
+
 dotenv.config();
+
 // https://astro.build/config
 export default defineConfig({
-    build:{
-        mode:"production"
+    build: {
+        mode: "production"
     },
-    site:"https://www.metaprolane.com/",
-    integrations: [react(),sitemap(),robotsTxt()],
+    site: "https://www.metaprolane.com/",
+    integrations: [react(), sitemap(), robotsTxt(), compress()],
 });
