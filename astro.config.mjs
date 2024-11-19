b2479d073e01dc3cb2fc95fceb3ce29850f17514
rajendran <rajendran@shamlatech.com>
2024-11-19 13:07:09 +0530

    added compress part

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,12 @@ dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-    build: {
-        mode: "production"
-    },
+    // build: {
+    //     mode: "production"
+    // },
     site: "https://www.metaprolane.com/",
-    integrations: [react(), sitemap(), robotsTxt()],
+    integrations: [react(), sitemap(), robotsTxt(),(await import("astro-compress")).default({
+        Image: false,
+        SVG: false,
+    })],
 });
