a18938fe87b7ff28ec3cc71ecc13f415c8563b0f
rajendran <rajendran@shamlatech.com>
2024-11-19 13:17:09 +0530

    removed compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,11 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import dotenv from 'dotenv';
 import robotsTxt from 'astro-robots-txt';
-import compress from 'astro-compress';
 
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-    // build: {
-    //     mode: "production"
-    // },
     site: "https://www.metaprolane.com/",
-    integrations: [react(), sitemap(), robotsTxt(),compress()],
+    integrations: [react(), sitemap(), robotsTxt()],
 });
