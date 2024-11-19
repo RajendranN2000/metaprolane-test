1f260ff3717cb5aef29817dbead91a7b9c0d6f88
rajendran <rajendran@shamlatech.com>
2024-11-19 13:01:34 +0530

    added build mode in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,9 @@ dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-    // build: {
-    //     mode: "production"
-    // },
+    build: {
+        mode: "production"
+    },
     site: "https://www.metaprolane.com/",
     integrations: [react(), sitemap(), robotsTxt()],
 });
